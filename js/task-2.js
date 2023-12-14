1ad02465d9e45cfa7673e4b55b5e94964aff32f9
irynahamoum <111897707+irynahamoum@users.noreply.github.com>
2023-12-14 22:20:05 +0100

    tasks 2 & 3

--- js/task-2.js
+++ js/task-2.js
@@ -7,6 +7,29 @@
 // removeItem(itemToRemove) — приймає рядок з назвою товару itemToRemove і видаляє його з масиву товарів у приватній властивості items об'єкта.
 // Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки коректності роботи. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
 
+class Storage {
+  #items;
+
+  constructor(items) {
+    this.#items = items;
+  }
+
+  getItems() {
+    return this.#items;
+  }
+
+  addItem(newItem) {
+    this.#items.push(newItem);
+  }
+
+  removeItem(itemToRemove) {
+    const index = this.#items.indexOf(itemToRemove);
+    if (index > -1) {
+      return this.#items.splice(index, 1);
+    }
+  }
+}
+
 const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
 console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem("Droid");
--- js/task-3.js
+++ js/task-3.js
@@ -8,6 +8,30 @@
 // padBoth(str) — отримує параметр str (рядок) і додає його на початок і в кінець значення приватної властивості  value об'єкта, який викликає цей метод.
 // Візьми код нижче з ініціалізацією екземпляра й викликами методів і встав його після оголошення класу для перевірки коректності роботи. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
 
+class StringBuilder {
+  #value;
+
+  constructor(initialValue) {
+    this.#value = initialValue;
+  }
+
+  getValue() {
+    return this.#value;
+  }
+
+  padEnd(str) {
+    this.#value = `${this.#value}${str}`;
+  }
+
+  padStart(str) {
+    this.#value = `${str}${this.#value}`;
+  }
+
+  padBoth(str) {
+    this.#value = `${str}${this.#value}${str}`;
+  }
+}
+
 const builder = new StringBuilder(".");
 console.log(builder.getValue()); // "."
 builder.padStart("^");
@@ -16,20 +40,3 @@ builder.padEnd("^");
 console.log(builder.getValue()); // "^.^"
 builder.padBoth("=");
 console.log(builder.getValue()); // "=^.^="
-
-// Оголошений клас StringBuilder
-// Властивість value у класі StringBuilder оголошена приватною
-// У класі StringBuilder оголошений метод getValue
-// Метод getValue повертає значення приватної властивості value екземпляра класу, який його викликає
-// У класі StringBuilder оголошений метод padEnd
-// Метод padEnd змінює значення приватної властивості value екземпляра класу, який його викликає
-// У класі StringBuilder оголошений метод padStart
-// Метод padStart змінює приватну властивість value екземпляра класу, який його викликає
-// У класі StringBuilder оголошений метод padBoth
-// Метод padBoth змінює значення приватної властивості value екземпляра класу, який його викликає
-// У результаті виклику new StringBuilder(".") значення приватної змінної builder — це об'єкт
-// Об'єкт builder не містить публічну властивість value
-// Перший виклик builder.getValue() одразу після ініціалізації екземпляра повертає рядок .
-// Другий виклик builder.getValue() після виклику builder.padStart("^") повертає рядок ^.
-// Третій виклик builder.getValue() після виклику builder.padEnd("^") повертає рядок ^.^
-// Четвертий виклик builder.getValue() після виклику builder.padBoth("=") повертає рядок =^.^=
